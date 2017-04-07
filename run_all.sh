5448bba706847f6c77ee3d9d0bdee9de9913fc16
Hiroaki Nakamura <hnakamur@gmail.com>
2017-04-07 06:14:23 +0000

    Run carbon-c-relay v2.6

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 for i in sv{01..100}; do
-  ./send_to_carbon_example -interval 1m -server-id $i -site-count 1000 &
+  ./send_to_carbon_example -graphite-port 2005 -interval 1m -server-id $i -site-count 1000 &
 done
 wait
---
+++ run_carbon-c-relay.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/sbin/relay -d -p 2005 -q 1000000 -B 1000000 -f /etc/carbon-c-relay.conf -l /var/log/carbon-c-relay/carbon-c-relay.log
